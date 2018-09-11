e77e5dde5d534d38b3975816def9cb6eeea3b570
Parag Zaveri <parag.g.zaveri@gmail.com>
2018-09-10 19:45:53 -0500

    creates tests for product

--- server/db/models/Product.js
+++ server/db/models/Product.js
@@ -2,20 +2,20 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Product = db.define('products', {
-	name: {
-		type: Sequelize.STRING,
-		allowNull: false
-	},
-	description: {
-		type: Sequelize.TEXT,
-		allowNull: false
-	},
-	price: {
-		type: Sequelize.FLOAT,
-		validate: {
-			min: 0
-		}
-	}
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false
+  },
+  price: {
+    type: Sequelize.FLOAT,
+    validate: {
+      min: 0
+    }
+  }
 })
 
 module.exports = Product
