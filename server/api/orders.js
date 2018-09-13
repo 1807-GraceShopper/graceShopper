8b1106b5d9bd80fd4ad10be9234a2ed33d7e0643
Parag Zaveri <parag.g.zaveri@gmail.com>
2018-09-13 16:18:05 -0500

    updates orders routes

--- server/api/orders.js
+++ server/api/orders.js
@@ -1,37 +1,69 @@
-const router = require('express').Router();
-const { Order, OrderItem } = require('../db/models');
+const router = require('express').Router()
+const {Order, OrderItem} = require('../db/models')
 
-module.exports = router;
+module.exports = router
 
 router.get('/', async (req, res, next) => {
-    try {
-        const orders = await Order.findAll({
-            include: [{model: OrderItem}]
-        });
-        res.json(orders);
-    } catch (err) {
-        next(err);
-    }
-});
+  try {
+    const orders = await Order.findAll({
+      include: [{model: OrderItem}]
+    })
+    res.json(orders)
+  } catch (err) {
+    next(err)
+  }
+})
 
 router.get('/:orderId', async (req, res, next) => {
-    const id = req.params.orderId;
-    try {
-        const order = await Order.findById(id, { 
-            include: [{model: OrderItem }]
-        });
-        res.json(order);
-    } catch (err) {
-        next(err);
-    }
-});
+  const id = req.params.orderId
+  try {
+    const order = await Order.findById(id, {
+      include: [{model: OrderItem}]
+    })
+    res.json(order)
+  } catch (err) {
+    next(err)
+  }
+})
 
 router.post('/', async (req, res, next) => {
-    try {
-        const newOrder = await Order.create({
-            name: req.order,
-        })
-    } catch (err) {
-        next(err);
-    }
+  try {
+    const newOrder = await Order.create({
+      price: req.body.price,
+      quantity: req.body.quantity,
+      timeOrdered: req.body.timeOrdered
+    })
+    res.json(newOrder)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:orderId', async (req, res, next) => {
+  try {
+    const orderUpdate = await Order.update(
+      {
+        price: req.body.price,
+        quantity: req.body.quantity,
+        timeOrdered: req.body.timeOrdered
+      },
+      {
+        returning: true,
+        where: {id: req.params.orderId}
+      }
+    )
+    res.json(orderUpdate)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:orderId', async (req, res, next) => {
+  try {
+    const orderToDestroy = await Order.findById(req.params.orderId)
+    orderToDestroy.destroy()
+    res.send(204).end()
+  } catch (err) {
+    next(err)
+  }
 })
