e97f3e33497896803090a8d57c860fc1e149447a
Sam Fortuna <saf1119@gmail.com>
2018-09-13 15:06:12 -0500

    still testing fix

---
+++ test-setup.js
@@ -0,0 +1,35 @@
+import Adapter from 'enzyme-adapter-react-16'
+import enzyme from 'enzyme'
+import db from './server/db'
+
+const adapter = new Adapter()
+enzyme.configure({adapter})
+
+/* https://stackoverflow.com/questions/32911630/how-do-i-deal-with-localstorage-in-jest-tests */
+class LocalStorageMock {
+  constructor() {
+    this.store = {}
+  }
+
+  clear() {
+    this.store = {}
+  }
+
+  getItem(key) {
+    return this.store[key] || null
+  }
+
+  setItem(key, value) {
+    this.store[key] = value.toString()
+  }
+
+  removeItem(key) {
+    delete this.store[key]
+  }
+}
+
+global.localStorage = new LocalStorageMock()
+
+beforeEach(() => {
+  return db.sync({force: true})
+})
