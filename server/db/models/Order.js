da762fe2636d64c8bb18b498ea4ec91f716a0299
Parag Zaveri <parag.g.zaveri@gmail.com>
2018-09-13 14:31:06 -0500

    Update Models To Include OrderItem and Shipping Info

--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -2,23 +2,15 @@ const Sequelize = require('sequelize')
 const db = require('../db')
 
 const Order = db.define('orders', {
-  price: Sequelize.ARRAY(Sequelize.FLOAT),
-  productId: Sequelize.ARRAY(Sequelize.INTEGER),
-  quantity: Sequelize.ARRAY(Sequelize.INTEGER),
+  price: {
+    type: Sequelize.INTEGER
+  },
+  quantity: {
+    type: Sequelize.INTEGER
+  },
   timeOrdered: {
     type: Sequelize.DATE,
     allowNull: false
-  },
-  shippingAddress: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-    validate: {
-      isEmail: true
-    }
   }
 })
 
