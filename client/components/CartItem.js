a1bc43062297b8af6777b9379f0d2c48bf607983
Alex Morson <amorson@conncoll.edu>
2018-09-14 13:47:27 -0500

    dummy for cart-view open. CartItem not functional yet

--- client/components/CartItem.js
+++ client/components/CartItem.js
@@ -1,3 +1,5 @@
+//NOTE: NOT DONE!
+
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { removeItemFromCart, incrementCartItem, decrementCartItem, setQuantityOfItem } from '../store/cart';
@@ -19,25 +21,47 @@ const mapDispatchToProps = dispatch => ({
 });
 
 class CartItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            cartItem: {}
+        }
+        this.onQuantityChange = this.onQuantityChange.bind(this);
+    }
+
+    componentDidMount() {
+        const cartItem = this.props.cart.find(item => item.id === Number(this.props.match.params.id));
+        this.setState({
+            cartItem
+        });
+    }
+
+    onQuantityChange(event) {
+        event.preventDefault();
+        let itemCopy = Object.assign({}, this.state.cartItem);
+        itemCopy.quantity = event.target.quantity;
+        this.setState({
+            cartItem : itemCopy
+        });
+        this.props.setQuantity(this.state.cartItem);
+    }
+
     render () {
         const { cart, products } = this.props;
         if (!cart || cart.length <= 0) return <div />
-        const cartItem = cart.find(citem => citem.id === this.props.match.params.id);
-        if (!cartItem) return (<div><h1>Cart Item does not exist</h1></div>);
+        const cartItem = cart.find(citem => citem.id === Number(this.props.match.params.id));
+        if (!this.state.cartItem) return (<div><h1>Cart Item does not exist</h1></div>);
         const associatedProduct = products.find(product => product.id === cartItem.productId);
         return(
-            <form onSubmit={this.handleSubmit} id="cartItemForm">
-                <li className="media">
-                    <div className="media-left">
-                        <img className="media-object" src={associatedProduct.photoUrl} alt="image" />
-                    </div>
-                    <div className="media-body">
-                        <h3><NavLink to={`/products/${cartItem.productId}`}>{associatedProduct.name}</NavLink></h3>
-                            <input type="number" name="quantity" value={cartItem.quantity} min={1} max={associatedProduct.quantity} step={1} />
-
-                    </div>
-                </li>
-            </form>
+            <div>
+                <form id="cartItemForm">
+                    <div> </div> {/* image div */}
+                    <div> </div> {/* name, delete option*/}
+                    <div> </div> {/* price*/}
+                    <div> </div> {/* quantity, put input here*/}
+                    <input type="number" name="quantity" value={this.state.cartItem.quantity} onChange={this.onQuantityChange} min={1} max={associatedProduct.quantity} step={1} />
+                </form>
+            </div>
         );
     }
 }
